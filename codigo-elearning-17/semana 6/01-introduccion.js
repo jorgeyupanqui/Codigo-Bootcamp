afacb71d8b6a2b6f2e76b74e234d1523e51ae431
Jorge Yupanqui <j.yupanqui.d@gmail.com>
2024-07-17 23:15:28 -0500

    added intro JS

---
+++ codigo-elearning-17/semana 6/01-introduccion.js
@@ -0,0 +1,43 @@
+console.log("Hola desde el archivo Javascript");
+
+//estructuras de datos
+//Variable
+let nombre = "Juan";
+
+let precioMonitor = 400;
+
+let precioSmartphone = 500;
+
+console.log(precioSmartphone);
+console.log(typeof precioSmartphone);
+
+let edad= 30;
+
+console.log("Edad: ",edad);
+console.log("Tipo Edad: ",typeof edad);
+
+let apellido= "Fernandez";
+console.log("apellido: ",apellido);
+console.log("Tipo apellido: ",typeof apellido);
+
+let meQuiere= false;
+console.log("meQuiere: ",meQuiere);
+console.log("Tipo meQuiere: ",typeof meQuiere);
+
+let noHayNada= null;
+console.log("noHayNada: ",noHayNada);
+console.log("Tipo noHayNada: ",typeof noHayNada);
+
+let porDefinir;
+console.log("porDefinir: ",porDefinir);
+console.log("Tipo porDefinir: ",typeof porDefinir);
+
+let anio = 2023;
+
+anio = 2024;
+
+console.log("anio: ",anio);
+
+anio = "DOS MIL VEINTICUATRO";
+
+console.log("otra vez anio: ",anio);
