4633e40c94b7dc97d1c7e52c2c51068139f5887a
Jorge Yupanqui <j.yupanqui.d@gmail.com>
2024-07-20 17:15:53 -0500

    Funciones Javascript

---
+++ codigo-elearning-17/semana 6/02-operaciones.js
@@ -0,0 +1,35 @@
+let precioMonitor = 400;
+let cpu = 300;
+let placamadre = 250;
+//Suma
+let precioTotal = precioMonitor + cpu + placamadre;
+
+console.log("Precio Total: ",precioTotal);
+
+let conServicio = 1000 + 50;
+console.log("Precio con Servicio: ",conServicio);
+
+let resta = 500 - 100;
+console.log("Resta: ",resta);
+
+let multiplicacion = 500 * 2;
+console.log("Multiplicacion: ",multiplicacion);
+
+let division = 1000 / 2;
+console.log("Division: ",division);
+
+let modulo = 10 % 3;
+console.log("Modulo: ",modulo);
+
+let nombre = "Juan";
+let apellido = "Fernandez";
+let saludo = "Hola " + nombre + " " + apellido;
+console.log("saludo: ",saludo);
+
+let suma = "1" + 1;
+console.log("suma: ",suma);
+console.log("Tipo suma: ",typeof suma);
+
+let otroSaludo = `Buen dia ${nombre} ${apellido} y mi edad es ${20+10}`;
+
+console.log("otroSaludo: ",otroSaludo);
---
+++ codigo-elearning-17/semana 6/03-funciones.js
@@ -0,0 +1,48 @@
+function calcularPrecioTotal(precio) {
+    let margenGanancia = 1.15;
+    let igv = 1.18;
+    let precioTotal = precio * margenGanancia * igv;
+    console.log("Precio Total: ", precioTotal);
+}
+
+calcularPrecioTotal(100);
+
+function calcularViaje(pasajes, estadia, comida) {
+    let precioTotal = pasajes + estadia + comida;
+    return precioTotal;
+}
+
+let resultado = calcularViaje(100, 50, 45);
+console.log("Resultado: ", resultado);
+
+
+let bebida = "Coca Cola";
+
+function grupoPizza(){
+    let aperitivo = "Papas a la Huancaina";
+    console.log(`voy a tomar algo de ${bebida}`);
+    console.log(`voy a comer ${aperitivo}`);
+}
+
+grupoPizza();
+//console.log(`yo tambien deseo ${aperitivo}`);
+
+function picarVegetales(){
+    console.log("picando Vegetales");
+}
+
+function granearArroz(){
+    console.log("graneando arroz");
+}
+
+function freirPolloYHuevo(){
+    console.log("freir pollo y huevos");
+}
+
+function prepararArrozConPollo(paso1,paso2,paso3){
+    paso1();
+    paso2();
+    paso3();
+}
+
+prepararArrozConPollo(picarVegetales,granearArroz,freirPolloYHuevo);
